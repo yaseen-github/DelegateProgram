0f60daa83fb68e3b50bf6920d842e601d5f7aa99
Muhammad Yaseen <muhammad.yaseen@avanza.pk>
2026-01-01 16:13:07 +0500

    Second commit

---
+++ DelegateProgram/EventHandlerProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProgram
+{
+    internal class EventHandlerProgram
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+            button.click += Button_Click;
+            button.InvokeClick();
+
+            Console.ReadLine();
+
+        }
+        static void Button_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button Clicked");
+        }
+    }
+
+    public class Button
+    {
+        public event EventHandler click;
+
+        public void InvokeClick()
+        {
+            click?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
